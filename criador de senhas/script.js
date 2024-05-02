ff337f809a8440cc79f9782167f6750b0da57630
maneiras <166811130+maneiras@users.noreply.github.com>
2024-05-02 10:45:56 +0100

    Add files via upload

---
+++ criador de senhas/script.js
@@ -0,0 +1,41 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container");
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!';
+let novaSenha = '';
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function() {
+  sizePassword.innerHTML = this.value;
+}
+
+buttonElement.onclick = function() {
+  generatePassword();
+}
+
+function generatePassword(){
+  let pass = '';
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+  
+  console.log(pass)
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
+}
+
+password.onclick = function() {
+  copyPassword();
+}
+
+function copyPassword(){
+  alert("Senha copiada com sucesso!")
+  navigator.clipboard.writeText(novaSenha);
+}
